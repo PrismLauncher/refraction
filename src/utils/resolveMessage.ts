e856c041080fd50df8920543799e8a7b15211f6e
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-01-17 13:11:36 +0800

    formatting issues

--- src/utils/resolveMessage.ts
+++ src/utils/resolveMessage.ts
@@ -6,11 +6,11 @@ import {
   EmbedBuilder,
   type Message,
   ThreadChannel,
-} from "discord.js";
+} from 'discord.js';
 
 function findFirstImage(message: Message): string | undefined {
   const result = message.attachments.find((attach) => {
-    return attach.contentType?.startsWith("image/");
+    return attach.contentType?.startsWith('image/');
   });
   if (result == undefined) {
     return undefined;
@@ -23,16 +23,20 @@ export async function expandDiscordLink(message: Message): Promise<void> {
   const re =
     /(https?:\/\/)?(?:canary\.|ptb\.)?discord(?:app)?\.com\/channels\/(?<server_id>\d+)\/(?<channel_id>\d+)\/(?<message_id>\d+)/g;
   let execResult = re.exec(message.content);
+
   while (!(execResult == null || execResult.groups == undefined)) {
     if (execResult.groups.server_id != message.guildId) {
       continue; // do not let the bot leak messages from one server to another
     }
+
     const channel = await message.guild?.channels.fetch(
       execResult.groups.channel_id
     );
+
     if (channel == undefined || channel == null || !channel.isTextBased()) {
       continue;
     }
+
     if (channel instanceof ThreadChannel) {
       if (
         !channel.parent?.members?.some((user) => user.id == message.author.id)
@@ -44,10 +48,12 @@ export async function expandDiscordLink(message: Message): Promise<void> {
         continue; // do not reveal a message to a user who can't see it
       }
     }
+
     try {
       const messageToShow = await channel.messages.fetch(
         execResult.groups.message_id
       );
+
       const builder = new EmbedBuilder()
         .setAuthor({
           name: `${messageToShow.author.username}#${messageToShow.author.discriminator}`,
@@ -55,28 +61,34 @@ export async function expandDiscordLink(message: Message): Promise<void> {
         })
         .setDescription(messageToShow.content)
         .setColor(Colors.Aqua);
+
       if (messageToShow.attachments.size > 0) {
-        let attachmentsString = "";
+        let attachmentsString = '';
         messageToShow.attachments.forEach((value) => {
           attachmentsString += `[${value.name}](${value.url}) `;
         });
-        builder.addFields({ name: "Attachments", value: attachmentsString });
+
+        builder.addFields({ name: 'Attachments', value: attachmentsString });
+
         const firstImage = findFirstImage(messageToShow);
         if (firstImage != undefined) {
           builder.setImage(firstImage);
         }
       }
-      const row = new ActionRowBuilder().addComponents(
+
+      const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
         new ButtonBuilder()
-          .setLabel("Jump to original message")
+          .setLabel('Jump to original message')
           .setStyle(ButtonStyle.Link)
           .setURL(messageToShow.url)
       );
+
       await message.channel.send({ embeds: [builder], components: [row] });
     } catch (e) {
       console.error(e);
       continue;
     }
+
     execResult = re.exec(message.content);
   }
 }
