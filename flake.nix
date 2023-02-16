ad63fedb4578f6da473ca56d4cf2cac28029952f
Sefa Eyeoglu <contact@scrumplex.net>
2023-02-17 00:30:17 +0100

    chore: add Flake and direnv
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Discord bot for Prism Launcher";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell { packages = with pkgs; [ yarn ]; };
+      });
+}
