a4d50dc6b96e107a646aba05fee9f37046c3a076
IThundxr <contact@ithundxr.dev>
2023-03-09 16:44:28 -0500

    woosh

--- src/utils/resolveMessage.ts
+++ src/utils/resolveMessage.ts
@@ -81,9 +81,9 @@ export async function expandDiscordLink(message: Message): Promise<void> {
           .setStyle(ButtonStyle.Link)
           .setURL(messageToShow.url),
         new ButtonBuilder()
-					.setCustomId('delete-message')
-					.setLabel('Delete')
-					.setStyle(ButtonStyle.Danger)
+          .setCustomId('delete-message')
+          .setLabel('Delete')
+          .setStyle(ButtonStyle.Danger)
       );
 
       await message.reply({
