7198e5b62053480cf2fc4b2e77b673cd737075aa
Sefa Eyeoglu <contact@scrumplex.net>
2023-10-11 14:18:06 +0200

    refactor: move steps into functions
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- src/server.ts
+++ src/server.ts
@@ -19,11 +19,33 @@ import config from './config';
 
 const MyOctokit = Octokit.plugin(paginateRest, throttling, retry);
 
-const makeRestAPI = (token = config.discord.botToken, bot = true) => {
+const octokit = new MyOctokit({
+  throttle: {
+    onRateLimit: (retryAfter, options) => {
+      octokit.log.warn(
+        `Request quota exhausted for request ${options.method} ${options.url}`
+      );
+
+      // Retry twice after hitting a rate limit error, then give up
+      if (options.request.retryCount <= 2) {
+        console.log(`Retrying after ${retryAfter} seconds!`);
+        return true;
+      }
+    },
+    onSecondaryRateLimit: (retryAfter, options, octokit) => {
+      // does not retry, only logs a warning
+      octokit.log.warn(
+        `Secondary quota detected for request ${options.method} ${options.url}`
+      );
+    },
+  },
+});
+
+const makeRestAPI = (accessToken: string) => {
   return new REST({
     version: '10',
-    authPrefix: bot ? 'Bot' : 'Bearer',
-  }).setToken(token);
+    authPrefix: 'Bearer',
+  }).setToken(accessToken);
 };
 
 // TODO: add state param
@@ -34,6 +56,38 @@ const generateAuthorizeUrl = () => {
   url.searchParams.append('response_type', 'code');
   url.searchParams.append('scope', config.discord.oauth2.scope);
   return url.toString();
+};
+
+const getTokensFromOAuth = async (code: string) => {
+  return await axios.post(
+    OAuth2Routes.tokenURL,
+    {
+      client_id: config.discord.clientId,
+      client_secret: config.discord.clientSecret,
+      code,
+      grant_type: 'authorization_code',
+      redirect_uri: config.discord.oauth2.redirectUri,
+      scope: config.discord.oauth2.scope,
+    },
+    { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+  );
+};
+
+const getGitHubConnections = async (rest: REST) => {
+  const connections = (await rest.get(
+    Routes.userConnections()
+  )) as RESTGetAPICurrentUserConnectionsResult;
+
+  return connections
+    .filter((connection) => connection.type == ConnectionService.GitHub)
+    .map((connection) => connection.id);
+};
+
+const getGitHubContributors = async (owner: string, repo: string) => {
+  return await octokit.paginate('GET /repos/{owner}/{repo}/contributors', {
+    owner,
+    repo,
+  });
 };
 
 export const listenApp = () => {
@@ -51,84 +105,35 @@ export const listenApp = () => {
       return;
     }
 
-    const tokenResponse = await axios.post(
-      OAuth2Routes.tokenURL,
-      {
-        client_id: config.discord.clientId,
-        client_secret: config.discord.clientSecret,
-        code,
-        grant_type: 'authorization_code',
-        redirect_uri: config.discord.oauth2.redirectUri,
-        scope: config.discord.oauth2.scope,
-      },
-      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
-    );
+    const tokenResponse = await getTokensFromOAuth(code.toString());
 
     // TODO: store tokens in Redis
 
-    const rest = makeRestAPI(tokenResponse.data.access_token, false);
+    const userRest = makeRestAPI(tokenResponse.data.access_token);
+    const githubUserIds = await getGitHubConnections(userRest);
 
-    const connections = (await rest.get(
-      Routes.userConnections()
-    )) as RESTGetAPICurrentUserConnectionsResult;
-    const githubUserIds = connections
-      .filter((connection) => connection.type == ConnectionService.GitHub)
-      .map((connection) => connection.id);
-
-    // Ask GitHub for contributions
     const metadata: RESTPutAPICurrentUserApplicationRoleConnectionJSONBody = {
-      metadata: {
-        contributed_launcher: 'true',
-        contributed_translations: 'true',
-      },
+      metadata: {},
     };
-
-    const octokit = new MyOctokit({
-      throttle: {
-        onRateLimit: (retryAfter, options) => {
-          octokit.log.warn(
-            `Request quota exhausted for request ${options.method} ${options.url}`
-          );
-
-          // Retry twice after hitting a rate limit error, then give up
-          if (options.request.retryCount <= 2) {
-            console.log(`Retrying after ${retryAfter} seconds!`);
-            return true;
-          }
-        },
-        onSecondaryRateLimit: (retryAfter, options, octokit) => {
-          // does not retry, only logs a warning
-          octokit.log.warn(
-            `Secondary quota detected for request ${options.method} ${options.url}`
-          );
-        },
-      },
-    });
 
     for (const repo of config.github.repos) {
       const key = `contributed_${repo.key}`;
       metadata.metadata![key] = 'false';
 
-      await octokit.paginate(
-        'GET /repos/{owner}/{repo}/contributors',
-        { owner: repo.owner, repo: repo.repo },
-        (response, done) => {
-          if (
-            response.data.find((contributor) =>
-              githubUserIds.includes(contributor.id!.toString())
-            )
-          ) {
-            done();
-            metadata.metadata![key] = 'true';
-          }
-          return response.data;
-        }
-      );
+      const contributors = await getGitHubContributors(repo.owner, repo.repo);
+
+      if (
+        contributors.find((contributor) =>
+          githubUserIds.includes(contributor.id!.toString())
+        )
+      ) {
+        metadata.metadata![key] = 'true';
+      }
     }
 
     // potentially add platform_name and platform_username
 
-    const discordResponse = await rest.put(
+    const discordResponse = await userRest.put(
       Routes.userApplicationRoleConnection(config.discord.clientId),
       {
         body: metadata,
