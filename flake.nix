52059da616d442f997e4f820d8c4640f4edf37c5
Sefa Eyeoglu <contact@scrumplex.net>
2023-10-11 14:41:49 +0200

    refactor(nix): switch to flake-parts, add redis
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- flake.nix
+++ flake.nix
@@ -8,16 +8,22 @@
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-root.url = "github:srid/flake-root";
+    proc-flake.url = "github:srid/proc-flake";
   };
 
   outputs = {
     flake-parts,
     pre-commit-hooks,
+    flake-root,
+    proc-flake,
     ...
   } @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
         pre-commit-hooks.flakeModule
+        flake-root.flakeModule
+        proc-flake.flakeModule
       ];
 
       perSystem = {
@@ -33,10 +39,14 @@
             excludes = ["flake.lock" "pnpm-lock.yaml"];
           };
         };
+        proc.groups.daemons.processes = {
+          redis.command = "${lib.getExe' pkgs.redis "redis-server"}";
+        };
         devShells.default = pkgs.mkShell {
           shellHook = ''
             ${config.pre-commit.installationScript}
           '';
+          nativeBuildInputs = [config.proc.groups.daemons.package];
           packages = with pkgs; [nodePackages.pnpm];
         };
         formatter = pkgs.alejandra;
