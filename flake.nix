7ab1002485d48064820f2b0e1e37be91848a0630
Sefa Eyeoglu <contact@scrumplex.net>
2023-10-11 14:41:49 +0200

    refactor(nix): switch to flake-parts
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- flake.nix
+++ flake.nix
@@ -3,39 +3,50 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
   outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
+    flake-parts,
     pre-commit-hooks,
     ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      checks = {
-        pre-commit-check = pre-commit-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            alejandra.enable = true;
-            prettier = {
-              enable = true;
-              excludes = ["flake.lock" "pnpm-lock.yaml"];
-            };
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        pre-commit-hooks.flakeModule
+      ];
+
+      perSystem = {
+        config,
+        lib,
+        pkgs,
+        ...
+      }: {
+        pre-commit.settings.hooks = {
+          alejandra.enable = true;
+          prettier = {
+            enable = true;
+            excludes = ["flake.lock" "pnpm-lock.yaml"];
           };
         };
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            ${config.pre-commit.installationScript}
+          '';
+          packages = with pkgs; [nodePackages.pnpm];
+        };
+        formatter = pkgs.alejandra;
       };
-      devShells.default = pkgs.mkShell {
-        inherit (self.checks.${system}.pre-commit-check) shellHook;
-        packages = with pkgs; [nodePackages.pnpm];
-      };
-    });
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+    };
 }
