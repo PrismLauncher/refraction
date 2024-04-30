4bb45b16c3211bd43f781e438d99188bff7c9e59
TheKodeToad <TheKodeToad@proton.me>
2024-05-01 00:01:10 +0100

    Better ping command

--- src/commands/general/ping.rs
+++ src/commands/general/ping.rs
@@ -1,11 +1,34 @@
+use std::time::{Duration, Instant};
+
 use crate::{Context, Error};
 
 use log::trace;
+use poise::CreateReply;
+
+const PING_PREFIX: &str = "<:catstareback:1078622789885497414> Pong!";
 
 /// Replies with pong!
 #[poise::command(slash_command, prefix_command, track_edits = true, ephemeral)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
 	trace!("Running ping command!");
-	ctx.say("Pong!").await?;
+
+	let start = Instant::now();
+	let response = ctx.say(PING_PREFIX).await?;
+
+	let rtt = start.elapsed().as_millis();
+	let gateway_ping = match ctx.ping().await {
+		Duration::ZERO => "Undetermined".to_string(),
+		duration => format!("{}ms", duration.as_millis()),
+	};
+
+	response
+		.edit(
+			ctx,
+			CreateReply::default().content(format!(
+				"{PING_PREFIX}\n\nRTT: {rtt}ms\nGateway: {gateway_ping}",
+			)),
+		)
+		.await?;
+
 	Ok(())
 }
