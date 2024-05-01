e949ff445ed729821bd3e2c671cf0c6381987d80
TheKodeToad <TheKodeToad@proton.me>
2024-05-01 01:43:39 +0100

    Fix outdated launcher check

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -8,3 +8,11 @@ pub fn embed_author_from_user(user: &User) -> CreateEmbedAuthor {
 			.unwrap_or_else(|| user.default_avatar_url()),
 	)
 }
+
+pub fn semver_split(version: &str) -> Vec<u32> {
+	version
+		.split('.')
+		.map(|s| s.parse().ok())
+		.flatten()
+		.collect::<Vec<u32>>()
+}
