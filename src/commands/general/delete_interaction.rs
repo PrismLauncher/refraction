1491705cbda6dc4f9a20a163590b1bfa182a97c6
TheKodeToad <TheKodeToad@proton.me>
2024-05-06 18:05:04 +0100

    Make requested changes + allow deleting with Manage Messages perm

--- src/commands/general/delete_interaction.rs
+++ src/commands/general/delete_interaction.rs
@@ -1,17 +1,30 @@
 use eyre::Result;
-use poise::serenity_prelude::Message;
+use poise::serenity_prelude::{InteractionType, Message, Permissions};
 
 use crate::{Context, Error};
 
 #[poise::command(context_menu_command = "Delete command", ephemeral)]
 pub async fn delete_interaction(ctx: Context<'_>, message: Message) -> Result<(), Error> {
+	const NO_COMMAND: &str = "❌ This message does not contain a command";
+
 	let Some(interaction) = &message.interaction else {
-		ctx.say("❌ This message does not contain a command")
-			.await?;
+		ctx.say(NO_COMMAND).await?;
 		return Ok(());
 	};
 
-	if interaction.user.id != ctx.author().id {
+	if interaction.kind != InteractionType::Command {
+		ctx.say(NO_COMMAND).await?;
+		return Ok(());
+	}
+
+	if !(interaction.user.id == ctx.author().id
+		|| interaction
+			.member
+			.as_ref()
+			.and_then(|m| m.permissions)
+			.map(|p| p.contains(Permissions::MANAGE_MESSAGES))
+			.unwrap_or(false))
+	{
 		ctx.say("❌ You cannot delete commands run by other users")
 			.await?;
 		return Ok(());
